fcc75c757b6a1b377d58834ce98abfa334ed6124
one-moonman <nicola.manoilov@gmail.com>
2022-11-04 18:19:19 +0100

    base template generation ready

--- index.js
+++ index.js
@@ -1,120 +1,56 @@
 #!/usr/bin/env node
 import fs from "fs";
-import { exec as notAsyncExec } from "child_process";
-import { promisify } from 'util';
-const exec = promisify(notAsyncExec);
-const readFile = promisify(fs.readFile);
-const appendFile = promisify(fs.appendFile);
-const writeFile = promisify(fs.watchFile);
+import { exec, execSync, spawn } from "node:child_process";
+import path from "path";
+import ora from 'ora';
+import { Command } from "commander";
+import { promisify } from "util";
+import * as url from 'url';
+const asyncExec = promisify(exec)
+const program = new Command();
 
-const args = process.argv.slice(2);
+program
+    .name('faster-cli')
+    .description('CLI to one-moonman`s NodeJS project template')
+    .version('1.0.0');
 
-switch (args[0]) {
-    case 'create':
-        await setupEnvironment()
-        break;
-    case 'module':
-        await createModule()
-        break;
-    default:
-        console.log("You have to provide function-name to execute")
-        process.exit(1)
-}
-
-async function setupEnvironment() {
-    const [prodDeps, devDeps] = await Promise.all([
-        exec('npm i fastify @fastify/swagger dotenv'),
-        exec('npm i --save-dev typescript ts-node @types/node nodemon esbuild esbuild-node-tsc json-schema-to-ts'),
-    ])
-    if (prodDeps.stderr || devDeps.stderr) {
-        console.log(prodDeps.stderr || devDeps.stderr)
-        process.exit(1)
-    }
-    // tsconfig.json
-    const tsconfig = () => appendFile('tsconfig.json', JSON.stringify({
-        "compilerOptions": {
-            "target": "esnext",
-            "module": "esnext",
-            "allowJs": true,
-            "removeComments": true,
-            "resolveJsonModule": true,
-            "typeRoots": [
-                "./node_modules/@types"
-            ],
-            "sourceMap": true,
-            "outDir": "dist",
-            "strict": true,
-            "lib": [
-                "esnext"
-            ],
-            "baseUrl": ".",
-            "forceConsistentCasingInFileNames": true,
-            "esModuleInterop": true,
-            "experimentalDecorators": true,
-            "emitDecoratorMetadata": true,
-            "moduleResolution": "Node",
-            "skipLibCheck": true,
-        },
-        "include": [
-            "src/**/*"
-        ],
-        "exclude": [
-            "node_modules"
-        ],
-    }))
-
-    // nodemon.json
-    const nodemon = () => appendFile('nodemon.json', JSON.stringify({
-        "watch": [
-            "src"
-        ],
-        "ignore": [
-            "src/**/*.test.ts"
-        ],
-        "ext": "ts,mjs,js,json,graphql",
-        "exec": "etsc && node ./dist/index.js",
-        "legacyWatch": true
-    }))
-
-    await Promise.all([tsconfig, nodemon])
-
-    // package.json scripts
-    let packageJsonBuffer = JSON.parse(fs.readFileSync('package.json').toString());
-    packageJsonBuffer.scripts = {
-        "tsn": "ts-node ./src/module/schema",
-        "build": "etsc ./src/index.ts",
-        "dev": "nodemon ./src/index.ts",
-        "start": "node ./dist/index.js"
-    }
-    fs.readFileSync('package.json', JSON.stringify(packageJsonBuffer))
-
-    // create index file
-    if (!fs.existsSync('src')) {
-        fs.mkdirSync('src');
-        fs.writeFileSync('src/index.ts', `import fastify from 'fastify';
-
-async function bootstrap() {
-    try {
-        const server = fastify();
-        server.get('/ping', async (request, reply) => ('pong'))
-        const address = await server.listen({ port: 8080 });
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-    }
-}
-
-bootstrap()`)
+function copyFolderSync(src, dest) {
+    if (!fs.existsSync(dest)) {
+        fs.mkdirSync(dest);
     }
 
-}
+    fs.readdirSync(src).forEach((entry) => {
+        const srcPath = path.join(src, entry);
+        const destPath = path.join(dest, entry);
+        const stat = fs.lstatSync(srcPath);
 
-async function createModule() {
-    try {
-        if (!fs.existsSync('src')) {
-            fs.mkdir('src');
+        if (stat.isFile()) {
+            fs.copyFileSync(srcPath, destPath);
+        } else if (stat.isDirectory()) {
+            copyFolderSync(srcPath, destPath);
+        } else if (stat.isSymbolicLink()) {
+            fs.symlinkSync(fs.readlinkSync(srcPath), destPath);
         }
-    } catch (err) {
-        console.error(err);
-    }
+    });
 }
+
+program
+    .command('generate')
+    .description('Generate project with template in given directory')
+    .argument('<string>', 'directory to generate')
+    .action(async str => {
+        const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+        const dest = path.join(process.cwd(), str)
+        const src = path.join(__dirname, 'template/app')
+        const spinner = ora('Loading files').start();
+        copyFolderSync(src, dest)
+        spinner.color = 'yellow';
+        spinner.text = 'Installing dependancies';
+        await asyncExec('pnpm i --prefix ./' + str);
+        spinner.stop()
+        console.log('Ready');
+    })
+
+program.parse();
+
+
